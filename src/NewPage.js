dc5f62576d3497756b16166fa7727f5a3f310a95
Anirban Aditya Halder <nabovtl@gmail.com>
2024-05-04 23:52:48 +0530

    api

--- src/NewPage.js
+++ src/NewPage.js
@@ -1,20 +1,41 @@
 import axios from "axios"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export default function NewPage(){
+
+    const [people, setPeople] = useState([])
 
     useEffect(() => {
       
       axios.get("https://swapi.dev/api/people")
       .then((res)=>{
+        
         console.log(res.data.results);
+        setPeople(res.data.results)
+
       })
 
     }, [])
     
     return(
         <div>
-            hello guys
+            {
+
+                // for insaan in people:
+                //      return(
+                //          <div>
+                //              {insaan.name}
+                //          </div>
+                // )
+
+                people.map( (insaan) => {
+                    return(
+                        <div>
+                            {insaan.name}
+                        </div>
+                    )
+                } )
+            }
         </div>
     )
 }
